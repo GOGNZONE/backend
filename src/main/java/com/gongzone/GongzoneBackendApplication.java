51093b7c2fa65dc3a1092733652896fb737a6ca5
k-dhyeon <kdhyeon4237@gmail.com>
2022-07-27 17:07:59 +0900

    initial commit

---
+++ src/main/java/com/gongzone/GongzoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gongzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongzoneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongzoneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gongzone/GongzoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gongzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongzoneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
