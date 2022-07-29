ad964dbc7199c369af05ac482cb5c743e161b20a
parkhanju <parkhanju95@gmail.com>
2022-07-29 10:03:56 +0900

    feat: Add release entity
    Add Release entity

---
+++ src/main/java/com/gongzone/entity/Release.java
@@ -0,0 +1,61 @@
+package com.gongzone.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 출고 엔티티
+ * @author Hanju Park
+ * @version 1.0
+ * */
+@Entity
+@Table(name = "`release`")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Release {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "release_id")
+	private Long releaseId;
+	
+	@Column(name = "release_date")
+	@NotNull(message = "release date cannot be null")
+	private LocalDate releaseDate;
+	
+	@Column(name = "release_description", columnDefinition = "TEXT")
+	private String releaseDescription;
+	
+	@Column(name = "release_quantity")
+	@NotNull(message = "release quantity cannot be null")
+	@ColumnDefault("0")
+	private int releaseQuantity;
+	
+	@Column(name = "release_total_price")
+	@NotNull(message = "release total price cannot be null")
+	@ColumnDefault("0")
+	private Long releaseTotalPrice;
+	
+	@Column(name = "release_type", columnDefinition = "ENUM('DELIVERY', 'VEHICLE')")
+	@Enumerated(value = EnumType.STRING)
+	@NotNull(message = "release type cannot be null")
+	private ReleaseType releaseType;
+	
+}
