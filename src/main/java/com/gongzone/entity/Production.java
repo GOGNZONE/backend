1f74866f468a86eba7c809c6ddc6bfd43039ba6d
parkhanju <parkhanju95@gmail.com>
2022-07-29 11:40:43 +0900

    feat: Add FK field(client)
    Add FK field(client)

--- src/main/java/com/gongzone/entity/Production.java
+++ src/main/java/com/gongzone/entity/Production.java
@@ -7,10 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.ColumnDefault;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -70,5 +74,10 @@ public class Production {
 	@Column(name = "production_date")
 	@NotNull(message = "production date cannot be null")
 	private LocalDate productionDate;
+	
+	@ManyToOne
+	@JoinColumn(name = "clientId", nullable = true)
+	@JsonIgnore
+	private Client client;
 	
 }
