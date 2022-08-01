170eb7718376f00af787b5f4d56a05d4241231df
parkhanju <parkhanju95@gmail.com>
2022-08-01 17:27:22 +0900

    feat: Add Component / Entity / Repository scan
    Add Component / Entity / Repository scan

--- src/main/java/com/gongzone/GongzoneBackendApplication.java
+++ src/main/java/com/gongzone/GongzoneBackendApplication.java
@@ -2,8 +2,14 @@ package com.gongzone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.gongzone.production.controller", "com.gongzone.production.service", "com.gongzone.production.dto" })
+@EntityScan(basePackages = { "com.gongzone.production.entity", "com.gongzone.client.entity" })
+@EnableJpaRepositories(basePackages = { "com.gongzone.production.repository" })
 public class GongzoneBackendApplication {
 
 	public static void main(String[] args) {
