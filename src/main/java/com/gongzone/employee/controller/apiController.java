d9798a972b72187f3be6e9f0437a7269227f1838
mingyu <75217853+gyu0714@users.noreply.github.com>
2022-08-02 18:34:11 +0900

    [CREATE] api controller

---
+++ src/main/java/com/gongzone/employee/controller/apiController.java
@@ -0,0 +1,13 @@
+package com.gongzone.employee.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/employee")
+public class apiController {
+
+}
