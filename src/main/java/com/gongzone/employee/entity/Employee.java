3f1d59d6dff5ee5fda9458157b380cd7211d6859
mingyu <75217853+gyu0714@users.noreply.github.com>
2022-08-02 18:35:18 +0900

    [CREATE] employee entity

---
+++ src/main/java/com/gongzone/employee/entity/Employee.java
@@ -0,0 +1,93 @@
+package com.gongzone.employee.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import com.gongzone.employee.dto.UpdateEmployeeDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 사원 엔티티
+ * @author kimmingyu
+ * @version 1.0
+ */
+@Entity
+@Table(name = "employee")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Employee {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "employee_id")
+	private Long employeeId;
+	
+	@Column(name = "employee_name", length = 20)
+	@NotNull(message = "name must not be null")
+	private String employeeName;
+	
+	@Column(name = "employee_password", length = 20)
+	@NotNull(message = "password must not be null")
+	private String employeePassword;
+	
+	@Column(name = "employee_address", length = 80)
+	@ColumnDefault("NULL")
+	private String employeeAddress;
+
+	@Column(name = "employee_email", length = 30, unique = true)
+	private String employeeEmail;
+	
+	@Column(name = "employee_phone", length = 18 ,unique = true)
+	private String employeePhone;
+	
+	@Column(name = "employee_hiredate")
+	@NotNull(message = "hiredate must not be null")
+	private String employeeHiredate;
+	
+	@Column(name = "employee_role")
+	@Enumerated(value = EnumType.STRING)
+	@NotNull(message = "employee role must not be null")
+	@ColumnDefault("'STAFF'")
+	private EmployeeRole employeeRole;
+	
+	@Column(name = "employee_image", columnDefinition = "TEXT")
+	@ColumnDefault("NULL")
+	private String employeeImage;
+	
+	/**
+	 * 사원 수정
+	 * @param {UpdateEmployeeDto}
+	 * @return void
+	 */
+	public void updateEmployeeInfo(UpdateEmployeeDto updateEmployeeDto) {
+		this.employeeName = getEmployeeName();
+		this.employeePhone = getEmployeePhone();
+		this.employeeAddress = getEmployeeAddress();
+		this.employeeEmail = getEmployeeEmail();
+	}
+	
+	/**
+	 * 패스워드 재설정
+	 * @param {employeePassword}
+	 * @return void
+	 */
+	public void updateEmployeePassword(String employeePassword) {
+		this.employeePassword = employeePassword;
+	}
+	
+}
---
+++ src/main/java/com/gongzone/employee/entity/EmployeeRole.java
@@ -0,0 +1,7 @@
+package com.gongzone.employee.entity;
+
+public enum EmployeeRole {
+	ADMIN,
+	MANAGER,
+	STAFF;
+}
