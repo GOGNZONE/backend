181b83150bdc4039d1dd6df57acd777216cf6e44
parkhanju <parkhanju95@gmail.com>
2022-08-02 17:56:48 +0900

    feat: Create ReleaseService
    Create ReleaseService

---
+++ src/main/java/com/gongzone/release/service/ReleaseService.java
@@ -0,0 +1,15 @@
+package com.gongzone.release.service;
+
+/**
+ * 출고 서비스 인터페이스
+ * @author Hanju Park
+ * @version 1.0
+ * */
+public interface ReleaseService {
+	
+	/**
+	 * 전체 출고 목록 조회
+	 * */
+//	public List<>
+	
+}
