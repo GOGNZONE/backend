7c42d9bf90d5a48208711bef0977a88033d173eb
parkhanju <parkhanju95@gmail.com>
2022-08-02 14:48:18 +0900

    feat: Add ProductionService
    Add ProductionService

--- src/main/java/com/gongzone/production/service/ProductionService.java
+++ src/main/java/com/gongzone/production/service/ProductionService.java
@@ -2,8 +2,8 @@ package com.gongzone.production.service;
 
 import java.util.List;
 
-import com.gongzone.production.dto.ProductionDTO;
-import com.gongzone.production.dto.ProductionFindAllDTO;
+import com.gongzone.production.dto.ProductionDto;
+import com.gongzone.production.dto.ProductionDtoDetail;
 
 /**
  * 생산 서비스 인터페이스
@@ -14,22 +14,36 @@ public interface ProductionService {
 	
 	/**
 	 *  전체 생산 목록 조회
-	 *  @return List<ProductionDTO>
+	 *  @return List<ProductionDtoDetail>
 	 */
-	public List<ProductionFindAllDTO> findAllProductions();
+	public List<ProductionDtoDetail> findAllProductions();
 	
 	/**
 	 * 생산 품목 코드(production_id)로 생산 품목 조회
 	 * @param { productionId }
 	 * @return ProductionDTO
 	 * */
-	public ProductionDTO findByProductionId(Long productionId);
+	public ProductionDto findByProductionId(Long productionId);
 	
 	/**
 	 * 생산 품목 등록
 	 * @param { productionDTO }
 	 * @return void
 	 * */
-	public void createProduction(ProductionDTO productionDTO);
-
+	public void insertProduction(ProductionDto productionDTO);
+	
+	/**
+	 * 생산 품목 코드(production_id)로 생산 품목 수정
+	 * @param { productionId, productionDTO }
+	 * @return void
+	 * */
+	public void updateProduction(Long productionId, ProductionDto productionDTO);
+	
+	/**
+	 * 생산 품목 코드(production_id)로 생산 품목 삭제
+	 * @param { productionId }
+	 * @return void
+	 * */
+	public void deleteProduction(Long productionId);
+	
 }
