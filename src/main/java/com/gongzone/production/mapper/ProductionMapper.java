322628a25769ed1387368c10ba3fa2065e5103a9
parkhanju <parkhanju95@gmail.com>
2022-08-02 17:06:59 +0900

    feat: Update ProductionMapper
    Update ProductionMapper

--- src/main/java/com/gongzone/production/mapper/ProductionMapper.java
+++ src/main/java/com/gongzone/production/mapper/ProductionMapper.java
@@ -7,22 +7,36 @@ import com.gongzone.production.dto.ProductionDto;
 import com.gongzone.production.entity.Production;
 
 /**
- * ProductionDTO 매핑 인터페이스
+ * 생산 품목 상세 조회, 생산 품목 등록 매핑 인터페이스
  * @author Hanju Park
  * @version 1.0
  * */
 @Mapper(componentModel = "spring")
 public interface ProductionMapper extends GenericMapper<ProductionDto, Production> {
 	
-//	@Override
-//	default Production toEntity(ProductionDto productionDto) {
-//		
-//		if(productionDto == null) {
-//			return null;
-//		}
-//		
-//		
-//		
-//	}
+	@Override
+	default Production toEntity(ProductionDto d) {
+		
+		if(d == null) {
+			return null;
+		}
+		
+		String productionName = d.getProductionName();
+		String productionBrandName = d.getProductionBrandName();
+		Long productionPrice = d.getProductionPrice();
+		int productionQuantity = d.getProductionQuantity();
+		String productionFile = d.getProductionFile();
+		String productionStandard = d.getProductionStandard();
+		String productionUnit = d.getProductionUnit();
+		String productionDescription = d.getProductionDescription();
+		String productionReleasedDate = d.getProductionReleasedDate();
+		String productionDate = d.getProductionDate();
+		
+		Production production = new Production(productionName, productionBrandName, productionPrice,
+				productionQuantity, productionFile, productionStandard, productionUnit,
+				productionDescription, productionReleasedDate, productionDate);
+		
+		return production;
+	}
 	
 }
