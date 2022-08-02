cc263f73ed03ebe1d657a587d3297b6aee68247b
parkhanju <parkhanju95@gmail.com>
2022-08-02 14:44:15 +0900

    feat: Update ProductionDto
    Update ProductionDto

--- src/main/java/com/gongzone/production/dto/ProductionDTO.java
+++ src/main/java/com/gongzone/production/dto/ProductionDTO.java
@@ -2,40 +2,30 @@ package com.gongzone.production.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
- * 생산 품목 코드(production_id)로 생산 품목 조회 DTO
+ * 생산 관련 요청/응답에 대한 Dto (전체 생산 품목 조회 제외)
  * @author Hanju Park
  * @version 1.0
  * */
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
-public class ProductionDTO {
+@Getter
+public class ProductionDto {
 	
 	private Long productionId;
-	
 	private String productionName;
-	
 	private String productionBrandName;
-	
 	private Long productionPrice;
-	
 	private int productionQuantity;
-	
 	private String productionFile;
-	
 	private String productionStandard;
-	
 	private String productionUnit;
-	
 	private String productionDescription;
-	
 	private String productionReleasedDate;
-	
 	private String productionDate;
 	
 }
