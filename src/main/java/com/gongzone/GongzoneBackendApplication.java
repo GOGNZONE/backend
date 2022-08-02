75742b272181abf1e56ad164d9b0e17cf708eb4a
mingyu <75217853+gyu0714@users.noreply.github.com>
2022-08-02 15:10:34 +0900

    [UPDATE] scans

--- src/main/java/com/gongzone/GongzoneBackendApplication.java
+++ src/main/java/com/gongzone/GongzoneBackendApplication.java
@@ -2,8 +2,12 @@ package com.gongzone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.gongzone.employee.entity"})
+@EnableJpaRepositories(basePackages = {"com.gongzone.employee.repository"})
 public class GongzoneBackendApplication {
 
 	public static void main(String[] args) {
