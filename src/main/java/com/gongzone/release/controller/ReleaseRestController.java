49114074e9bac99e5311f8fc22faca3ee5efc89a
parkhanju <parkhanju95@gmail.com>
2022-08-02 17:59:05 +0900

    feat: Create ReleaseRestController
    Create ReleaseRestController

---
+++ src/main/java/com/gongzone/release/controller/ReleaseRestController.java
@@ -0,0 +1,21 @@
+package com.gongzone.release.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 출고 컨트롤러
+ * @author Hanju Park
+ * @version 1.0
+ * */
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/release")
+public class ReleaseRestController {
+	
+	
+}
