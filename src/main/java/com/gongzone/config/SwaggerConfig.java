41c3a9a59b68290d7903b0f78de84e6923b75886
mingyu <75217853+gyu0714@users.noreply.github.com>
2022-08-03 18:26:46 +0900

    [UPDATE] swagger config

--- src/main/java/com/gongzone/config/SwaggerConfig.java
+++ src/main/java/com/gongzone/config/SwaggerConfig.java
@@ -1,26 +1,16 @@
 package com.gongzone.config;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.google.common.base.Predicates;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -28,44 +18,37 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("GONGZONE")
+				.description("GONGZONE API")
+				.version("1.0.0")
+				.build();
+	}
+	
+	private Set<String> getConsumeContentTypes() {
+		Set<String> consumes = new HashSet<>();
+		consumes.add("application/json;charset=UTF-8");
+		consumes.add("applicationi/x-www-form-urlencoded");
+		return consumes;
+	}
+	
+	private Set<String> getProduceContentTypes() {
+		Set<String> produces = new HashSet<>();
+		produces.add("application/json;charset=UTF-8");
+		return produces;
+	}
+	
 	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)//
-				.select()//
-				.apis(RequestHandlerSelectors.any())//
-				.paths(Predicates.not(PathSelectors.regex("/error")))//
-				.build()//
-				.apiInfo(metadata())//
-				.useDefaultResponseMessages(false)//
-				.securitySchemes(Collections.singletonList(apiKey()))
-				.securityContexts(Collections.singletonList(securityContext()))
-				.tags(new Tag("users", "Operations about users"))//
-				.genericModelSubstitutes(Optional.class);
-	}
-
-	private SecurityContext securityContext() {
-		return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
-	}
-
-	private List<SecurityReference> defaultAuth() {
-		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-		authorizationScopes[0] = authorizationScope;
-		return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
-	}
-
-	private ApiKey apiKey() {
-		return new ApiKey("Authorization", "Authorization", "header");
-	}
-
-	private ApiInfo metadata() {
-		return new ApiInfoBuilder()//
-				.title("JSON Web Token Authentication API")//
-				.description(
-						"Gongzone 소규모 공장, 공방 관리 시스템")//
-				.version("1.0.0")//
-				.license("MIT License").licenseUrl("http://opensource.org/licenses/MIT")//
-				.contact(new Contact(null, null, "lagyu99@gmail.com"))//
+	public Docket commonApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.consumes(getConsumeContentTypes())
+				.produces(getProduceContentTypes())
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage(
+						"com.gongzone.employee.apiController"))
+				.paths(PathSelectors.ant("/api/**"))
 				.build();
 	}
 
