b7eb275576c0f37eedbc95601cab2e4c0dfb4bf5
parkhanju <parkhanju95@gmail.com>
2022-08-03 17:48:00 +0900

    feat: Update ReleaseRestController
    update ReleaseRestController

--- src/main/java/com/gongzone/release/apiController/ReleaseRestController.java
+++ src/main/java/com/gongzone/release/apiController/ReleaseRestController.java
@@ -3,15 +3,17 @@ package com.gongzone.release.apiController;
 import java.util.List;
 
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gongzone.release.dto.ReleaseDetailsVehicleDto;
-import com.gongzone.release.dto.ReleaseListDto;
+import com.gongzone.release.dto.ReleaseDetailsDto;
+import com.gongzone.release.dto.ReleaseDto;
 import com.gongzone.release.service.ReleaseServiceImpl;
 
 import lombok.RequiredArgsConstructor;
@@ -32,34 +34,53 @@ public class ReleaseRestController {
 	
 	/**
 	 * 전체 출고 목록 조회
-	 * @return List<ReleaseListDto>
+	 * @return List<ReleaseDto>
 	 * */
 	@GetMapping("/list")
-	public List<ReleaseListDto> findAllReleases() {
+	public List<ReleaseDto> findAllReleases() {
 		return releaseServiceImpl.findAllReleases();
 	}
 	
 	/**
 	 * 출고 코드(release_id)로 출고 상세 조회
 	 * @param { releaseId }
-	 * @return ReleaseDto
+	 * @return ReleaseDetailsDto
 	 * */
-//	@GetMapping("/{releaseId}")
-//	public ReleaseDetailsDto findByReleaseId(@PathVariable Long releaseId) {
-//		return releaseServiceImpl.findByReleaseId(releaseId);
-//	}
+	@GetMapping("/{releaseId}")
+	public ReleaseDetailsDto findByReleaseId(@PathVariable Long releaseId) {
+		return releaseServiceImpl.findByReleaseId(releaseId);
+	}
 	
 	/**
 	 * 출고 등록
+	 * @param { releaseDto }
+	 * @return void
 	 * */
-//	@PostMapping("")
-//	public void insertRelease(@Validated @RequestBody final ReleaseDto releaseDto )
+	@PostMapping("")
+	public void insertRelease(@Validated @RequestBody final ReleaseDto releaseDto) {
+		log.info("@param releaseDto : {}", releaseDto);
+		releaseServiceImpl.insertRelease(releaseDto);
+	}
 	
 	/**
 	 * 출고 코드(release_id)로 출고 수정
+	 * @param { releaseId, releaseDto }
+	 * @return void
 	 * */
+	@PutMapping("/{releaseId}")
+	public void updateRelease(@PathVariable Long releaseId, @Validated @RequestBody final ReleaseDto releaseDto) {
+		log.info("@param releaseId : {}", releaseId);
+		log.info("@param releaseDto : {}", releaseDto);
+		releaseServiceImpl.updateRelease(releaseId, releaseDto);
+	}
 	
 	/**
 	 * 출고 코드(release_id)로 출고 삭제
+	 * @param { releaseId }
+	 * @return void
 	 * */
+	@DeleteMapping("/{releaseId}")
+	public void deleteRelease(@PathVariable Long releaseId) {
+		releaseServiceImpl.deleteRelease(releaseId);
+	}
 }
