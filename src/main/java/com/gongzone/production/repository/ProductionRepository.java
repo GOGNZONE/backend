32496fad9d51a266db194b05e4d4323342877517
parkhanju <parkhanju95@gmail.com>
2022-08-03 09:53:37 +0900

    feat: Update ProductionRepository
    remove Repository import

--- src/main/java/com/gongzone/production/repository/ProductionRepository.java
+++ src/main/java/com/gongzone/production/repository/ProductionRepository.java
@@ -1,7 +1,6 @@
 package com.gongzone.production.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.gongzone.production.entity.Production;
 
