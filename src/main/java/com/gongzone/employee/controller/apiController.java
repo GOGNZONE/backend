7a24307aa0fb68d76e82af23e1c52345763de58a
mingyu <75217853+gyu0714@users.noreply.github.com>
2022-08-03 09:42:27 +0900

    [CREATE] employee controller

--- src/main/java/com/gongzone/employee/controller/apiController.java
+++ src/main/java/com/gongzone/employee/controller/apiController.java
@@ -1,7 +1,14 @@
 package com.gongzone.employee.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gongzone.employee.dto.EmployeeListDto;
+import com.gongzone.employee.service.EmployeeServiceImpl;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,4 +17,10 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/employee")
 public class apiController {
 
+	private final EmployeeServiceImpl employeeService;
+	
+	@GetMapping("/list")
+	public ResponseEntity<List<EmployeeListDto>> findAllEmployee() {
+		return ResponseEntity.ok(employeeService.findAllEmployee());
+	}
 }
