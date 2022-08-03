7738a69274c42fa5dadc5d9ab75725fde365dd2f
parkhanju <parkhanju95@gmail.com>
2022-08-03 17:46:07 +0900

    feat: Create DeliveryDto
    create DeliveryDto

---
+++ src/main/java/com/gongzone/delivery/dto/DeliveryDto.java
@@ -0,0 +1,25 @@
+package com.gongzone.delivery.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 택배 Dto
+ * @author Hanju Park
+ * @version 1.0
+ * */
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class DeliveryDto {
+	
+	private Long deliveryId;
+	
+	private String deliveryCompanyName;
+	
+	private String deliveryTrackingNumber;
+	
+}
