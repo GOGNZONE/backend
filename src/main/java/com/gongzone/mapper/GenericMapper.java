fe24d71d9a3b2a5396cbe348dfc781df7e949b50
parkhanju <parkhanju95@gmail.com>
2022-08-03 10:04:32 +0900

    feat: Update GenericMapper
    update GenericMapper

--- src/main/java/com/gongzone/mapper/GenericMapper.java
+++ src/main/java/com/gongzone/mapper/GenericMapper.java
@@ -2,6 +2,10 @@ package com.gongzone.mapper;
 
 import java.util.List;
 
+import org.mapstruct.BeanMapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
 /**
  * 모든 Entity ↔ DTO 변환에 적용시킬 GenericMapper 인터페이스
  * @author Hanju Park
@@ -13,4 +17,7 @@ public interface GenericMapper<D, E> {
 	
 	List<D> toDtoList(List<E> entityList);
 	List<E> toEntityList(List<D> dtoList);
+
+	@BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+	void updateProduction(D dto, @MappingTarget E entity);
 }
