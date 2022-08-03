6f9cb318b58146f75b213ebe7bfa38d81e16ca26
gyu0714 <lagyu99@gmail.com>
2022-08-03 20:25:50 +0900

    [DONE] retired employee repository

--- src/main/java/com/gongzone/employee/repository/RetiredEmployeeRepository.java
+++ src/main/java/com/gongzone/employee/repository/RetiredEmployeeRepository.java
@@ -3,7 +3,6 @@ package com.gongzone.employee.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.gongzone.employee.entity.Employee;
 import com.gongzone.employee.entity.RetiredEmployee;
 
 @Repository
