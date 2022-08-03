ad33a2e3aa25cb9f54d71249472e475c0d15018f
parkhanju <parkhanju95@gmail.com>
2022-08-03 17:49:03 +0900

    feat: Create ReleaseDto
    create ReleaseDto

---
+++ src/main/java/com/gongzone/release/dto/ReleaseDto.java
@@ -0,0 +1,33 @@
+package com.gongzone.release.dto;
+
+import com.gongzone.release.entity.ReleaseType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 전체 출고 목록 조회 Dto
+ * @author Hanju Park
+ * @version 1.0
+ * */
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ReleaseDto {
+	
+	private Long releaseId;
+	
+	private String releaseDate;
+	
+	private String releaseDescription;
+	
+	private int releaseQuantity;
+	
+	private Long releaseTotalPrice;
+	
+	private ReleaseType releaseType;
+	
+}
