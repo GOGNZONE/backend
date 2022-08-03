5b25e71fb97b73238933e4fb503fe501f3ea7fa7
parkhanju <parkhanju95@gmail.com>
2022-08-03 17:26:24 +0900

    Revert "feat: clientRegisteredDate type change"
    This reverts commit 69eab85eef2487a98337d1edac3fe45ff2eb513c.

--- src/main/java/com/gongzone/release/mapper/ReleaseListMapper.java
+++ src/main/java/com/gongzone/release/mapper/ReleaseListMapper.java
@@ -3,15 +3,15 @@ package com.gongzone.release.mapper;
 import org.mapstruct.Mapper;
 
 import com.gongzone.mapper.GenericMapper;
-import com.gongzone.release.dto.ReleaseDto;
+import com.gongzone.release.dto.ReleaseListDto;
 import com.gongzone.release.entity.Release;
 
 /**
- * ReleaseListDto ↔ Release
+ * ReleaseListMapper 매핑 인터페이스
  * @author Hanju Park
  * @version 1.0
  * */
 @Mapper(componentModel = "spring")
-public interface ReleaseMapper extends GenericMapper<ReleaseDto, Release> {
+public interface ReleaseListMapper extends GenericMapper<ReleaseListDto, Release> {
 	
 }
