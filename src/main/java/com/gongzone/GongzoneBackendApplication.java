fed423937f9a3354ff5b453fcd22e330a283a02e
parkhanju <parkhanju95@gmail.com>
2022-08-04 05:13:24 +0900

    feat: Update GongzoneBackendApplication
    add entityscan "com.gongzone.employee.entity"

--- src/main/java/com/gongzone/GongzoneBackendApplication.java
+++ src/main/java/com/gongzone/GongzoneBackendApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.gongzone.production.apiController", "com.gongzone.production.service", "com.gongzone.production.dto" })
-@EntityScan(basePackages = { "com.gongzone.production.entity", "com.gongzone.client.entity" })
+@EntityScan(basePackages = { "com.gongzone.production.entity", "com.gongzone.client.entity", "com.gongzone.employee.entity" })
 @EnableJpaRepositories(basePackages = { "com.gongzone.production.repository" })
 public class GongzoneBackendApplication {
 
