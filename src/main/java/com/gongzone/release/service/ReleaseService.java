56ae88389b2a6a6bd911344e5c01dec3603fc485
parkhanju <parkhanju95@gmail.com>
2022-08-03 11:12:34 +0900

    feat: Update ReleaseService
    update ReleaseService

--- src/main/java/com/gongzone/release/service/ReleaseService.java
+++ src/main/java/com/gongzone/release/service/ReleaseService.java
@@ -1,5 +1,10 @@
 package com.gongzone.release.service;
 
+import java.util.List;
+
+import com.gongzone.release.dto.ReleaseDetailsVehicleDto;
+import com.gongzone.release.dto.ReleaseListDto;
+
 /**
  * 출고 서비스 인터페이스
  * @author Hanju Park
@@ -9,7 +14,21 @@ public interface ReleaseService {
 	
 	/**
 	 * 전체 출고 목록 조회
+	 * @return List<ReleaseListDto>
 	 * */
-//	public List<>
+	public List<ReleaseListDto> findAllReleases();
+	
+	/**
+	 * 출고 코드(release_id)로 출고 조회
+	 * @param { releaseId }
+	 * @return ReleaseDto
+	 * */
+//	public ReleaseDetailsDto findByReleaseId(Long releaseId);
+	
+	/**
+	 * 출고 등록
+	 * @return void
+	 * */
+//	public void insertRelease()
 	
 }
