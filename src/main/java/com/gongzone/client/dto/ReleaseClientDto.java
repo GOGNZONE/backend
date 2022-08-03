17f46be7a3e7e6ecb315a5f128c445596d6c0af2
parkhanju <parkhanju95@gmail.com>
2022-08-03 17:24:03 +0900

    feat: Create ReleaseClientDto
    Create ReleaseClientDto

---
+++ src/main/java/com/gongzone/client/dto/ReleaseClientDto.java
@@ -0,0 +1,33 @@
+package com.gongzone.client.dto;
+
+import com.gongzone.employee.entity.Employee;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 출고 상세 조회 거래처 정보 Dto
+ * @author Hanju Park
+ * @version 1.0
+ * */
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ReleaseClientDto {
+	
+	private Long clientId;
+	
+	private String clientName;
+	
+	private String clientManager;
+	
+	private String clientTel;
+	
+	private String clientAddress;
+	
+	private Employee employee;
+	
+}
