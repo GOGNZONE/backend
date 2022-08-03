95502dc83cb08c98a45d090331ff0c6e68f93f33
gyu0714 <lagyu99@gmail.com>
2022-08-03 20:25:17 +0900

    initial commit

--- src/main/java/com/gongzone/GongzoneBackendApplication.java
+++ src/main/java/com/gongzone/GongzoneBackendApplication.java
@@ -3,14 +3,11 @@ package com.gongzone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
 
 @EnableJpaAuditing
 @EnableAspectJAutoProxy
