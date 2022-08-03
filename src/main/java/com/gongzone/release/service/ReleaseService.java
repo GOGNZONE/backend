6c5e47f92ddfa2b65d0f80ecfb7b42cd6999aa2f
parkhanju <parkhanju95@gmail.com>
2022-08-03 17:51:03 +0900

    feat: Update ReleaseService
    update ReleaseService

--- src/main/java/com/gongzone/release/service/ReleaseService.java
+++ src/main/java/com/gongzone/release/service/ReleaseService.java
@@ -2,8 +2,8 @@ package com.gongzone.release.service;
 
 import java.util.List;
 
-import com.gongzone.release.dto.ReleaseDetailsVehicleDto;
-import com.gongzone.release.dto.ReleaseListDto;
+import com.gongzone.release.dto.ReleaseDetailsDto;
+import com.gongzone.release.dto.ReleaseDto;
 
 /**
  * 출고 서비스 인터페이스
@@ -14,21 +14,36 @@ public interface ReleaseService {
 	
 	/**
 	 * 전체 출고 목록 조회
-	 * @return List<ReleaseListDto>
+	 * @return List<ReleaseDto>
 	 * */
-	public List<ReleaseListDto> findAllReleases();
+	public List<ReleaseDto> findAllReleases();
 	
 	/**
 	 * 출고 코드(release_id)로 출고 조회
 	 * @param { releaseId }
-	 * @return ReleaseDto
+	 * @return ReleaseDetailsDto
 	 * */
-//	public ReleaseDetailsDto findByReleaseId(Long releaseId);
+	public ReleaseDetailsDto findByReleaseId(Long releaseId);
 	
 	/**
 	 * 출고 등록
+	 * @param { releaseDto }
 	 * @return void
 	 * */
-//	public void insertRelease()
+	public void insertRelease(ReleaseDto releaseDto);
+	
+	/**
+	 * 출고 코드(release_id)로 출고 수정
+	 * @param { releaseId, releaseDto }
+	 * @return void
+	 * */
+	public void updateRelease(Long releaseId, ReleaseDto releaseDto);
+	
+	/**
+	 * 출고 코드(release_id)로 출고 삭제
+	 * @param { releaseId }
+	 * @return void
+	 * */
+	public void deleteRelease(Long releaseId);
 	
 }
