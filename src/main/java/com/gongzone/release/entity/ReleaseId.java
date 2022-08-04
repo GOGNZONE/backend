99826d798e041407b6e63e7d85bcaae4c9d20a82
parkhanju <parkhanju95@gmail.com>
2022-08-04 11:02:21 +0900

    feat: Update ReleaseId
    update ReleaseId

--- src/main/java/com/gongzone/release/entity/ReleaseId.java
+++ src/main/java/com/gongzone/release/entity/ReleaseId.java
@@ -4,6 +4,11 @@ import java.io.Serializable;
 
 import com.gongzone.production.entity.Production;
 
+/**
+ * Production 엔티티와 Release 엔티티 식별관계에 의한 맵핑(복합키)
+ * @author Hanju Park
+ * @version 1.0
+ * */
 public class ReleaseId implements Serializable {
 	
 	private static final long serialVersionUID = -2929789292155268166L;
