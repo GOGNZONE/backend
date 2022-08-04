bcb0da9ad813a7a2d49b32fcfb27e3423bf6a895
parkhanju <parkhanju95@gmail.com>
2022-08-04 09:42:12 +0900

    feat: Update Client entity
    update Client entity

--- src/main/java/com/gongzone/client/entity/Client.java
+++ src/main/java/com/gongzone/client/entity/Client.java
@@ -1,5 +1,7 @@
 package com.gongzone.client.entity;
 
+import java.time.LocalDate;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
@@ -47,6 +49,7 @@ public class Client {
 	@NotNull(message = "client manager must not be null")
 	private String clientManager;
 	
+	
 	@Column(name = "client_tel", length = 20)
 	@NotNull(message = "client tel must not be null")
 	private String clientTel;
@@ -58,15 +61,15 @@ public class Client {
 	@CreatedDate
 	@Column(name = "client_registered_date")
 	@NotNull(message = "client registered date must not be null")
-	private String clientRegisteredDate;
+	private LocalDate clientRegisteredDate;
 	
 	@Column(name = "client_file", columnDefinition = "TEXT")
 	private String clientFile;
 	
-//	@ManyToOne
-//	@JoinColumn(name = "employeeId", nullable = true)
-//	@JsonIgnore
-//	private Employee employee;
+	@ManyToOne
+	@JoinColumn(name = "employeeId", nullable = true)
+	@JsonIgnore
+	private Employee employee;
 	
 	/**
 	 * 거래처 정보 수정 메소드
