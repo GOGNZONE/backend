4fa8a1a520fef262b1624d91091ad36ae9041ca4
parkhanju <parkhanju95@gmail.com>
2022-08-06 10:03:15 +0900

    feat: Update ProductionRepository
    update ProductionRepository

--- src/main/java/com/gongzone/production/repository/ProductionRepository.java
+++ src/main/java/com/gongzone/production/repository/ProductionRepository.java
@@ -1,6 +1,8 @@
 package com.gongzone.production.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.gongzone.production.entity.Production;
 
@@ -10,5 +12,5 @@ import com.gongzone.production.entity.Production;
  * @version 1.0
  * */
 public interface ProductionRepository extends JpaRepository<Production, Long>{
-
+	
 }
