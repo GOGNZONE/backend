5897b9499204fc99a366cb255bc7b86ff1263281
parkhanju <parkhanju95@gmail.com>
2022-08-06 14:01:18 +0900

    feat: Create DeliveryRepository
    create DeliveryRepository

---
+++ src/main/java/com/gongzone/release/repository/DeliveryRepository.java
@@ -0,0 +1,14 @@
+package com.gongzone.release.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gongzone.release.entity.Delivery;
+
+/**
+ * 배송 리포지토리
+ * @author Hanju Park
+ * @version 1.0
+ * */
+public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+
+}
