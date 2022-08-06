0c1942077c4ac320c900a15b980b1b5adc4dd373
parkhanju <parkhanju95@gmail.com>
2022-08-06 12:03:36 +0900

    feat: Update ReleaseId
    update ReleaseId

--- src/main/java/com/gongzone/release/entity/ReleaseId.java
+++ src/main/java/com/gongzone/release/entity/ReleaseId.java
@@ -4,26 +4,31 @@ import java.io.Serializable;
 
 import com.gongzone.production.entity.Production;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * Production 엔티티와 Release 엔티티 식별관계에 의한 맵핑(복합키)
  * @author Hanju Park
  * @version 1.0
  * */
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
 public class ReleaseId implements Serializable {
 	
-	private static final long serialVersionUID = -2929789292155268166L;
-	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	private Long releaseId;
 	
 	private Production production;
 	
-	@Override
-	public int hashCode() {
-		return super.hashCode();
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		return super.equals(obj);
-	}
 }
