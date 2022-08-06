6dee868a15585c622ab09da019c29bd1c48d3966
parkhanju <parkhanju95@gmail.com>
2022-08-06 11:56:44 +0900

    feat: Update Delivery entity
    Delivery FK related annotations

--- src/main/java/com/gongzone/delivery/entity/Delivery.java
+++ src/main/java/com/gongzone/delivery/entity/Delivery.java
@@ -5,11 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-//import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-
-//import com.gongzone.release.entity.Release;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -40,8 +37,5 @@ public class Delivery {
 	@Column(name = "delivery_tracking_number", length = 30)
 	@NotNull(message = "delivery tracking number cannot be null")
 	private String deliveryTrackingNumber;
-	
-//	@OneToOne(mappedBy = "delivery")
-//    private Release release;
 	
 }
