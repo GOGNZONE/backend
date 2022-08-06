7f9a257fa2ad54392c8e03fd1dfa14754b6f7bbf
parkhanju <parkhanju95@gmail.com>
2022-08-06 12:02:53 +0900

    feat: Create ReleaseInsertUpdateDto
    create ReleaseInsertUpdateDto

---
+++ src/main/java/com/gongzone/release/dto/ReleaseInsertUpdateDto.java
@@ -0,0 +1,39 @@
+package com.gongzone.release.dto;
+
+import com.gongzone.delivery.entity.Delivery;
+import com.gongzone.production.entity.Production;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 출고 등록, 수정 Dto
+ * @author Hanju Park
+ * @version 1.0
+ * */
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class ReleaseInsertUpdateDto {
+	
+	private Long releaseId;
+	
+	private String releaseDate;
+	
+	private String releaseDescription;
+	
+	private int releaseQuantity;
+	
+	private Long releaseTotalPrice;
+	
+	private String releaseType;
+	
+	private Production production;
+	
+	private Delivery delivery;
+	
+}
