e9d6de4619c622ceda19466eb5e5d4ba151a7887
parkhanju <parkhanju95@gmail.com>
2022-08-06 12:01:59 +0900

    feat:  Add and modify Release related Dto
    add and modify Release related Dto

--- src/main/java/com/gongzone/release/dto/ReleaseDto.java
+++ src/main/java/com/gongzone/release/dto/ReleaseDto.java
@@ -1,12 +1,16 @@
 package com.gongzone.release.dto;
 
+import com.gongzone.client.dto.ReleaseClientDto;
+import com.gongzone.delivery.dto.DeliveryDto;
+import com.gongzone.production.dto.ReleaseProductionDto;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
- * 전체 출고 목록 조회 Dto
+ * 출고 상세 조회 Dto
  * @author Hanju Park
  * @version 1.0
  * */
@@ -25,5 +29,16 @@ public class ReleaseDto {
 	private int releaseQuantity;
 	
 	private Long releaseTotalPrice;
+	
+	private String releaseType;
+	
+	// Client Info
+	private ReleaseClientDto releaseClientDto;
+	
+	// Production Info
+	private ReleaseProductionDto releaseProductionDto;
+	
+	// Delivery Info
+	private DeliveryDto deliveryDto;
 	
 }
