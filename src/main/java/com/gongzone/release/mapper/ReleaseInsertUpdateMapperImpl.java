7b1075d871f0eb0a18fb3ea586b78f108a4c3691
parkhanju <parkhanju95@gmail.com>
2022-08-06 14:00:29 +0900

    feat: Update ReleaseInsertUpdateMapperImpl
    update ReleaseInsertUpdateMapperImpl

--- src/main/java/com/gongzone/release/mapper/ReleaseInsertUpdateMapperImpl.java
+++ src/main/java/com/gongzone/release/mapper/ReleaseInsertUpdateMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-08-05T14:05:22+0900",
+    date = "2022-08-06T13:11:18+0900",
     comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.jar, environment: Java 17.0.3 (Eclipse Adoptium)"
 )
 @Component
