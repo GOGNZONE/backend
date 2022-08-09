0c203fcb5cc74085cc3c40126d0aefedbc89ab54
mingyu <75217853+gyu0714@users.noreply.github.com>
2022-08-09 11:19:16 +0900

    [UPDATE] upload file name

--- src/main/java/com/gongzone/s3/service/S3Service.java
+++ src/main/java/com/gongzone/s3/service/S3Service.java
@@ -28,7 +28,7 @@ public class S3Service {
 
     public String uploadFile(MultipartFile file) {
         File fileObj = convertMultiPartFileToFile(file);
-        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+        String fileName = file.getOriginalFilename();
         s3Client.putObject(new PutObjectRequest(bucketName, fileName, fileObj));
         fileObj.delete();
         return "File uploaded : " + fileName;
