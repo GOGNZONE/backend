9cd15b8f1463fd033571f87a688ca2f5b7d836e8
k-dhyeon <kdhyeon4237@gmail.com>
2022-09-17 20:41:40 +0900

    2041

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -25,8 +25,8 @@ echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-sudo chmod 777 /home/ubuntu/file/logs/
-sudo chmod 777 /home/ubuntu/file/logs/logback.log
+sudo chmod 755 /home/ubuntu/file/logs/
+sudo chmod 666 /home/ubuntu/file/logs/logback.log
 sudo chown -R ubuntu:ubuntu /home/ubuntu/file/logs
 sudo chown sudo chmod 755 $JAR_NAME
 sudo chmod 755 $REPOSITORY
