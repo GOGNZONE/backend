40648d899dc1f80cf130b30297beae239ecb5531
k-dhyeon <106370891+k-dhyeon@users.noreply.github.com>
2022-09-19 10:25:22 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ cp $REPOSITORY/*.jar /home/ubuntu/file/
 
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl jar | awk '{print $1}')https://github.com/GOGNZONE/backend/blob/develop/scripts/deploy.sh
+CURRENT_PID=$(pgrep -fl jar | awk '{print $1}')
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID" 
 if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
